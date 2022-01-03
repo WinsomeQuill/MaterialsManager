028ee9948cdea42fa803f01ea92dc4e0ea32fe29
WinsomeQuill <artemqa.belyakov07@gmail.com>
2022-01-03 19:46:16 +0700

    Добавьте файлы проекта.

---
+++ MaterialsManager/MainWindow.xaml.cs
@@ -0,0 +1,149 @@
+﻿using ICSharpCode.SharpZipLib.BZip2;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using Ookii.Dialogs.Wpf;
+
+namespace MaterialsManager
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void GoToBz2(string path)
+        {
+            FileInfo fileToBeZipped = new FileInfo(path);
+            FileInfo zipFileName = new FileInfo(string.Concat(fileToBeZipped.FullName, ".bz2"));
+            using (FileStream fileToBeZippedAsStream = fileToBeZipped.OpenRead())
+            {
+                using (FileStream zipTargetAsStream = zipFileName.Create())
+                {
+                    BZip2.Compress(fileToBeZippedAsStream, zipTargetAsStream, true, 4096);
+                }
+            }
+        }
+        public void ExtractBz2(string path)
+        {
+            using (FileStream fileToDecompressAsStream = new FileInfo(path).OpenRead())
+            {
+                string[] f = path.Split('.');
+                string decompressedFileName = null;
+                for (int i = 0; i < f.Length; i++)
+                {
+                    if ((f.Length - 2) == i)
+                    {
+                        decompressedFileName += $".";
+                    }
+
+                    if ((f.Length - 1) != i)
+                    {
+                        decompressedFileName += f[i];
+                    }
+                }
+
+                using (FileStream decompressedStream = File.Create(decompressedFileName))
+                {
+                    BZip2.Decompress(fileToDecompressAsStream, decompressedStream, true);
+                }
+            }
+        }
+        private void ButtonToBz2Run_Click(object sender, RoutedEventArgs e)
+        {
+            string path = TextBoxToBz2DirInput.Text;
+            bool ?removeoriginalfile = CheckBoxToBz2RemoveOriginalFile.IsChecked;
+            if (path.Length == 0)
+            {
+                MessageBox.Show("Path is empty!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if(!Directory.Exists(path))
+            {
+                MessageBox.Show("Path not exists!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            foreach(string item in Directory.GetFiles(path))
+            {
+                GoToBz2(item);
+                if ((bool)removeoriginalfile)
+                {
+                    File.Delete(item);
+                }
+            }
+        }
+        private void ButtonUnBz2Run_Click(object sender, RoutedEventArgs e)
+        {
+            string path = TextBoxUnBz2DirInput.Text;
+            bool? removeoriginalfile = CheckBoxUnBz2RemoveOriginalFile.IsChecked;
+            if (path.Length == 0)
+            {
+                MessageBox.Show("Path is empty!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (!Directory.Exists(path))
+            {
+                MessageBox.Show("Path not exists!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            foreach (string item in Directory.GetFiles(path))
+            {
+                try
+                {
+                    ExtractBz2(item);
+                }
+                catch (NotSupportedException)
+                {
+                    MessageBox.Show("Bz2 files not found!", "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+                
+                if ((bool)removeoriginalfile)
+                {
+                    File.Delete(item);
+                }
+            }
+        }
+        private void ButtonToBz2OpenFolder_Click(object sender, RoutedEventArgs e)
+        {
+            VistaFolderBrowserDialog dialog = new VistaFolderBrowserDialog();
+            dialog.SelectedPath = Directory.GetCurrentDirectory();
+            bool? success = dialog.ShowDialog();
+            if (success == true)
+            {
+                TextBoxToBz2DirInput.Text = dialog.SelectedPath;
+            }
+        }
+        private void ButtonUnBz2OpenFolder_Click(object sender, RoutedEventArgs e)
+        {
+            VistaFolderBrowserDialog dialog = new VistaFolderBrowserDialog();
+            dialog.SelectedPath = Directory.GetCurrentDirectory();
+            bool? success = dialog.ShowDialog();
+            if (success == true)
+            {
+                TextBoxUnBz2DirInput.Text = dialog.SelectedPath;
+            }
+        }
+    }
+}
